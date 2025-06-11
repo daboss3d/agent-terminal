d6fa2607e6c1d523aacd4486b35d07e6f243cbb0
Carlos Correia <daboss3d@gmail.com>
2025-06-11 18:31:18 +0100

    working build and scripts

---
+++ ai.py
@@ -0,0 +1,13 @@
+from llm.ollama import  hello , query_ollama_stream
+from llm.test import  hello_test 
+
+def main():
+    print("Hello from agent-terminal!")
+    hello_test()
+    hello()
+    txt = query_ollama_stream("http://10.1.1.62:11434/api/generate", "how to list directories ?")
+    print(txt)
+
+
+if __name__ == "__main__":
+    main()
