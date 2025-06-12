8d71ae43051ad03e058c5d28e08e41135852ef48
Carlos Correia <daboss3d@gmail.com>
2025-06-12 16:57:43 +0100

    apenai is working

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-from lib.llm.ollama import  hello , query_ollama_stream
+
+import sys
+from lib.llm.ollama import query_ollama_stream, query_ollama 
 from lib.llm.test import  hello_test 
 
+from lib.ai import main as agent
+
+
+def contains_substring(text : str, aList : list):
+    for element in aList:
+        if text in element:
+            # print("--------------FOUND ",text," IN ",element)
+            return True
+    return False
+
+
 def main():
-    print("Hello from agent-terminal!")
-    hello_test()
-    hello()
-    txt = query_ollama_stream("http://10.1.1.62:11434/api/generate", "how to list directories ?")
-    print(txt)
+
+    agent()
+    return
+
+    # print("[Main] ------------------ parameters: ", len(sys.argv[1:]))
+    # print(sys.argv[1:])
+    # print("[Main]---------------------------------")
+
+    # args = sys.argv[1:]
+
+    # if contains_substring("--stream",args):
+    #     query_ollama_stream("http://10.1.1.62:11434/api/generate", "how to list directories ?")
+    # else :
+    #     txt = query_ollama("http://10.1.1.62:11434/api/generate", "how to list directories ?")
+    #     print(txt)
+
+    # print("Hello from agent-terminal!")
+    # hello_test()
+    # hello()
+    # txt = query_ollama_stream("http://10.1.1.62:11434/api/generate", "how to list directories ?")
+    # print(txt)
 
 
 if __name__ == "__main__":
